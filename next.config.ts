afe3118e19745f830a0847af1d54839c3f9dd0bf
Ayush Khatri <khatriayush2003@gmail.com>
2025-03-12 01:34:46 +0530

    feat: Major ui revamp

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.aceternity.com',
-        pathname: '/*',
-      },
-    ],
-  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
