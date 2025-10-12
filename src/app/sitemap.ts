29f771156aec1f97d6c8c4b90bf41736c78b6a10
Ayush Khatri <108052277+ayush-khatrii@users.noreply.github.com>
2025-10-12 19:20:58 +0530

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,31 +3,31 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://ayushkhatri.site',
+      url: 'https://ayushkhatri.in',
       lastModified: new Date().toISOString(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://ayushkhatri.site/about',
+      url: 'https://ayushkhatri.in/about',
       lastModified: new Date().toISOString(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: 'https://ayushkhatri.site/blogs',
+      url: 'https://ayushkhatri.in/blogs',
       lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
     {
-      url: 'https://ayushkhatri.site/contact',
+      url: 'https://ayushkhatri.in/contact',
       lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 0.5,
     },
     {
-      url: 'https://ayushkhatri.site/projects   ',
+      url: 'https://ayushkhatri.in/projects   ',
       lastModified: new Date().toISOString(),
       changeFrequency: 'weekly',
       priority: 0.5,
